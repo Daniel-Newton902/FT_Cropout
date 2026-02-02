97222dad9c762e6991ef61bd050dc0dd547e0a18
Daniel-Newton902 <daniel2008newton@gmail.com>
2026-02-02 14:50:10 +0000

    Week2Lab in progress
    The code of the game mode base class isn't working, I may want to try again from scratch.

---
+++ Source/FT_Cropout/Public/C_Spawner.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "C_Spawner.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FT_CROPOUT_API AC_Spawner : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
